e685fdfe5f760174e9e6475862737775c47f2b7e
MiaMMogensen <eaa24mimm@students.eaaa.dk>
2025-10-03 10:15:46 +0200

    navbar-change

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -3,9 +3,9 @@ import { NavLink } from "react-router";
 export default function Nav() {
   return (
     <nav>
-      <NavLink to="/">Home</NavLink>
-      <NavLink to="/about">About</NavLink>
-      <NavLink to="/contact">Contact</NavLink>
+      <NavLink to="/">Hjem</NavLink>
+      <NavLink to="/about">Om</NavLink>
+      <NavLink to="/contact">Kontakt</NavLink>
     </nav>
   );
 }
