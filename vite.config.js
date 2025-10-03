3b57c0b9204e57166a9a06addf110951a0a9745f
MiaMMogensen <eaa24mimm@students.eaaa.dk>
2025-10-03 10:05:54 +0200

    Update base paths for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -5,12 +5,12 @@ import react from "@vitejs/plugin-react-swc";
 export default defineConfig(({ command }) => {
   const config = {
     plugins: [react()],
-    base: "/"
+    base: "/",
   };
 
   // Change base path when building for production
   if (command !== "serve") {
-    config.base = "/react-vite-spa/"; // 👈 Replace with your GitHub repository name
+    config.base = "/collectify/"; // 👈 Replace with your GitHub repository name
   }
 
   return config;
