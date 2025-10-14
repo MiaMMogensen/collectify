5b70b64c2daf0303ad74dd09fe2d08904081d37f
MiaMMogensen <eaa24mimm@students.eaaa.dk>
2025-10-14 10:25:29 +0200

    feat: add landing page with navigation and new SVG icon

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,8 @@
 import { Routes, Route, Navigate } from "react-router";
-import HomePage from "./pages/HomePage";
 import AboutPage from "./pages/AboutPage";
 import ContactPage from "./pages/ContactPage";
 import Header from "./components/Header";
-import Nav from "./components/Nav";
+import LandingPage from "./pages/LandingPage";
 
 export default function App() {
   return (
@@ -11,13 +10,13 @@ export default function App() {
       <Header />
       <main>
         <Routes>
-          <Route path="/" element={<HomePage />} />
+          <Route path="/" element={<LandingPage />} />
           <Route path="/about" element={<AboutPage />} />
           <Route path="/contact" element={<ContactPage />} />
+          {/* Tilføj login og signup sider*/}
           <Route path="*" element={<Navigate to="/" />} />
         </Routes>
       </main>
-      <Nav />
     </>
   );
 }
---
+++ src/pages/LandingPage.jsx
@@ -0,0 +1,35 @@
+import landingPage from "../assets/img/landingpage.png";
+import arrowIcon from "../assets/img/arrow.svg";
+import { Link } from "react-router";
+
+export default function LandingPage() {
+  return (
+    <main>
+      <h1 className="landing-page-title">Get Collectify-ing Today!</h1>
+      <div className="landing-page-container">
+        <div className="gradient-frame">
+          <img
+            className="landing-page-image"
+            src={landingPage}
+            alt="Landing Page image"
+          />
+        </div>
+      </div>
+
+      <div className="landing-page-btns">
+        <Link to="/login" className="login-btn" aria-label="Login">
+          Login
+        </Link>
+        <Link to="/signup" className="get-started-btn" aria-label="Get Started">
+          Get Started
+          <img
+            src={arrowIcon}
+            alt="white arrow icon"
+            className="arrow-icon"
+            aria-hidden="true"
+          />
+        </Link>
+      </div>
+    </main>
+  );
+}
