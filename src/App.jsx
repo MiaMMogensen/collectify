12f539a53fffa8dd30269d67db7d6b75ebb68935
markuseaaa <eaa24markuk@students.eaaa.dk>
2025-10-16 14:45:09 +0200

    chore: clean up unused code and improve file structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import { Routes, Route, Navigate } from "react-router";
-import AboutPage from "./pages/AboutPage";
-import ContactPage from "./pages/ContactPage";
 import Header from "./components/Header";
 import LandingPage from "./pages/LandingPage";
 import Login from "./pages/Login";
@@ -25,8 +23,6 @@ export default function App() {
       <main>
         <Routes>
           <Route path="/" element={<LandingPage />} />
-          <Route path="/about" element={<AboutPage />} />
-          <Route path="/contact" element={<ContactPage />} />
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
           <Route path="/createcollection" element={<CreateCollection />} />
